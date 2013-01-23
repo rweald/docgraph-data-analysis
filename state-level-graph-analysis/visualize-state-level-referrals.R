257441d109cc8c921b2c7ed29d48b8ca2a5fcf2a
Ryan Weald <ryan@weald.com>
2013-01-22 16:31:05 -0800

    Add a bit of extra sexiness to the graph

--- state-level-graph-analysis/visualize-state-level-referrals.R
+++ state-level-graph-analysis/visualize-state-level-referrals.R
@@ -8,10 +8,10 @@ states <- c("AL","AK","AZ","AR","CA","CO","CT","DE","DC",
             "NC","ND","OH","OK","OR","MD","MA","MI","MN","MS","MO",
             "PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
 
-referrals <- read.csv("~/code/docgraph-data-analysis/refer.2011.sample2.csv", header=F)
+referrals <- read.csv("~/code/docgraph-data-analysis/refer.2011.sample2.csv", header = F)
 colnames(referrals) <- c("doc1", "doc2", "number.of.patients")
 
-npi.to.state <- read.csv("~/Downloads/Physician provider ID (NPI) data dump/npi-to-state.csv", as.is = T)
+npi.to.state <- read.csv("~/Downloads/Physician provider ID (NPI) data dump/npi-to-state.csv", as.is  =  T)
 colnames(npi.to.state) <- c("npi.number", "state")
 ddply(npi.to.state, c("state"), summarize, docs = nrow(state))
 
@@ -19,7 +19,7 @@ tmp1 <- merge(referrals, npi.to.state, by.x = "doc1", by.y = "npi.number")
 refs.with.state <- merge(tmp1, npi.to.state, by.x = "doc2", by.y = "npi.number")
 colnames(refs.with.state) <- c("doc2", "doc1", "number.of.patients", "doc1.state", "doc2.state")
 
-patients.by.state <- ddply(refs.with.state, c("doc1.state", "doc2.state"), summarize, patients=sum(number.of.patients))
+patients.by.state <- ddply(refs.with.state, c("doc1.state", "doc2.state"), summarize, patients = sum(number.of.patients))
 
 #binaryRange <- function(x){(x-min(x))/(max(x)-min(x))}
 
@@ -30,10 +30,17 @@ out.of.state <- subset(patients.by.state, doc1.state != doc2.state)
 out.of.state <- subset(out.of.state, (doc1.state %in% states) & (doc2.state %in% states))
 
 default.color <- "ivory"
+
 ggplot(out.of.state, aes(doc1.state, doc2.state)) +
-  geom_tile(aes(fill=log(patients)), colour = default.color) + 
-  scale_fill_gradient(low=default.color, high="orange") +
-  theme(plot.background=element_rect(fill="white", colour=default.color),
-        panel.background = element_rect(fill=default.color, colour=default.color),
-        axis.ticks = element_blank())
+  geom_tile(aes(fill = log(patients)), colour = default.color) + 
+  scale_fill_gradient(low = default.color, 
+                      high = "orange",
+                      name = "Log Scale Number of Patients") +
+  labs(x = "Referring State",
+       y = "State Being Referred To",
+       title = "DocGraph Inter-State Referrals") +
+  theme(plot.background = element_rect(fill = "white", colour = default.color),
+        panel.background = element_rect(fill = default.color, colour = default.color),
+        axis.ticks = element_blank(),
+        axis.text.x = element_text(angle = 90))
 
