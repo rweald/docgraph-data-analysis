8fbff9d095ad8ac39ac3a79e56e104dc58b0990b
Ryan Weald <ryan@weald.com>
2013-01-26 12:53:55 -0800

    Get the styling right for the graph

--- state-level-graph-analysis/visualize-state-level-referrals.R
+++ state-level-graph-analysis/visualize-state-level-referrals.R
@@ -2,6 +2,26 @@ library(ggplot2)
 library(plyr)
 library(maps)
 
+blank_theme <- function() {
+  return(
+         theme(axis.line=element_blank(),
+               axis.text.x=element_blank(),
+               axis.text.y=element_blank(),
+               axis.ticks=element_blank(),
+               axis.title.x=element_blank(),
+               axis.title.y=element_blank(),
+               panel.background=element_blank(),
+               panel.border=element_blank(),
+               panel.grid.major=element_blank(),
+               panel.grid.minor=element_blank(),
+               plot.background=element_blank(),
+               legend.text = element_text(size = 16),
+               legend.title = element_text(size = 16),
+               legend.position = "right",
+               plot.title = element_text(size=24))
+         )
+}
+
 # Combine the referrals data with the NPI database to get state location of Doc
 referrals <- read.csv("~/code/docgraph-data-analysis/refer.2011.sample2.csv", header = F)
 colnames(referrals) <- c("doc1", "doc2", "number.of.patients")
@@ -31,20 +51,17 @@ colnames(degree.by.state) <- c("state", "indegree", "outdegree")
 degree.by.state$ration.out.vs.in <- degree.by.state$outdegree / degree.by.state$indegree
 
 #Get state data in a format that can be merged with Maps state names
-
 state.names <- data.frame(region = tolower(state.name), abbreviation = state.abb)
 degree.by.state <- merge(degree.by.state, state.names, by.x = "state", by.y = "abbreviation")
 
+################################################################################
 #choropleth plot of indegree vs outdegree for each state
-
 state_df <- map_data("state")
+
 choropleth <- merge(state_df, degree.by.state, by = "region", all.x = T)
 choropleth[with(choropleth, is.na(ration.out.vs.in)),]$ration.out.vs.in <- 1
 choropleth <- choropleth[order(choropleth$order), ]
 
-#choropleth$ratio_d <- cut(choropleth$ration.out.vs.in, 
-                          #breaks = seq(from = min(choropleth$ration.out.vs.in), 
-                                       #to = max(choropleth$ration.out.vs.in), by = 0.01))
 
 choropleth$ratio_d <- cut(choropleth$ration.out.vs.in, breaks = 9)
 
@@ -52,9 +69,18 @@ ggplot(choropleth, aes(long, lat, group = group)) +
   geom_polygon(aes(fill= ration.out.vs.in), size=0.2) +
   scale_fill_gradient(low = rgb(252,187,136, maxColorValue=255),
                       high = rgb(215,73,25, maxColorValue=255),
-                      name = "Log Scale Number of Patients")
+                      name = "Ration Outbound/Inbound Referrals") +
+  labs(title = "Ration of Outbound vs Inboud Referrals") +
+  blank_theme() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 50),
+        legend.text = element_text(size = 30),
+        legend.title = element_text(size = 30))
+
+ggsave("outbound-vs-inbound-choropleth-dev.png", width=40, height = 25)
 
 
+################################################################################
 # Matrix plot of Inter-State referrals
 default.color <- "ivory"
 
