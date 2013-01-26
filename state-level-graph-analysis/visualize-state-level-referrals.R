8b67dd5178138b1ca697de6836ccd31b42a4ce66
Ryan Weald <ryan@weald.com>
2013-01-26 14:50:23 -0800

    Add some more plots to on state level data

--- state-level-graph-analysis/visualize-state-level-referrals.R
+++ state-level-graph-analysis/visualize-state-level-referrals.R
@@ -2,6 +2,42 @@ library(ggplot2)
 library(plyr)
 library(maps)
 
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 blank_theme <- function() {
   return(
          theme(axis.line=element_blank(),
@@ -20,6 +56,17 @@ blank_theme <- function() {
                legend.position = "right",
                plot.title = element_text(size=24))
          )
+}
+
+custom_theme <- function() {
+  return(
+    theme(legend.position = "bottom",
+          plot.title = element_text(size = 50),
+          legend.text = element_text(size = 60),
+          legend.background = element_rect(size = 60),
+          legend.key.width = unit(0.075, "npc"),
+          legend.title = element_text(size = 40))
+  )
 }
 
 # Combine the referrals data with the NPI database to get state location of Doc
@@ -75,6 +122,124 @@ steps <- seq(from = round(min(choropleth$ration.out.vs.in), digits = 2), to = ro
 
 ggplot(choropleth, aes(long, lat, group = group)) +
   geom_polygon(aes(fill= ration.out.vs.in), size=0.2) +
+  scale_fill_gradient(low = rgb(252,187,136, maxColorValue=255),
+                      high = rgb(215,73,25, maxColorValue=255),
+                      name = "",
+                      labels = steps,
+                      breaks = steps) +
+  labs(title = "Ration of Outbound vs Inboud Referrals") +
+  blank_theme() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 50),
+        legend.text = element_text(size = 60),
+        legend.background = element_rect(size = 60),
+        legend.key.width = unit(0.075, "npc"),
+        legend.title = element_text(size = 40))
+
+ggsave("outbound-vs-inbound-choropleth-dev.png", width=40, height = 25, units = "in")
+
+################################################################################
+#basic statistical plots of state level data
+
+
+png("general-statistical-plots.png", width=40, height = 25, units = "in", res = 72)
+
+text.theme <- theme(plot.title = element_text(size = 35),
+                    axis.title.x = element_text(size = 30),
+                    axis.title.y = element_text(size = 30),
+                    axis.text.x = element_text(size = 20),
+                    axis.text.y = element_text(size = 20))
+
+
+g1 <- ggplot(all.by.state) +
+  geom_density(aes(percent.in.state), fill = "lightblue") +
+  ggtitle("Distribution - Percent of Total Referrals That Stay In State - Distribution") +
+  text.theme
+
+g2 <- ggplot(all.by.state) +
+  geom_density(aes(percent.from.out.of.state), fill = "lightcoral") +
+  ggtitle("Distribution - Percent of Total Referrals From Out of State") + 
+  text.theme
+
+g3 <- ggplot(all.by.state) +
+  geom_density(aes(percent.leaving.state), fill = "mediumaquamarine") +
+  ggtitle("Distribution - Percent of Total Referrals That Leave State - Distribution") +
+  text.theme
+
+multiplot(g1, g2, g3, cols = 2)
+
+dev.off()
+
+################################################################################
+#choropleth plot of indegree vs outdegree for each state
+state_df <- map_data("state")
+
+choropleth <- merge(state_df, all.by.state, by = "region", all.x = T)
+choropleth[with(choropleth, is.na(ration.out.vs.in)),]$ration.out.vs.in <- 1
+choropleth <- choropleth[order(choropleth$order), ]
+
+steps <- seq(from = round(min(choropleth$ration.out.vs.in), digits = 2), to = round(max(choropleth$ration.out.vs.in), digits = 2), by = 0.05)
+
+ggplot(choropleth, aes(long, lat, group = group)) +
+  geom_polygon(aes(fill= ration.out.vs.in), size=1, colour = "black") +
+  scale_fill_gradient(low = rgb(252,187,136, maxColorValue=255),
+                      high = rgb(215,73,25, maxColorValue=255),
+                      name = "",
+                      labels = steps,
+                      breaks = steps) +
+  labs(title = "Ration of Outbound vs Inboud Referrals") +
+  blank_theme() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 50),
+        legend.text = element_text(size = 60),
+        legend.background = element_rect(size = 60),
+        legend.key.width = unit(0.075, "npc"),
+        legend.title = element_text(size = 40))
+
+ggsave("outbound-vs-inbound-choropleth-dev.png", width=40, height = 25, units = "in")
+
+
+################################################################################
+#choropleth plot of percent in-state
+state_df <- map_data("state")
+
+choropleth <- merge(state_df, all.by.state, by = "region", all.x = T)
+choropleth[with(choropleth, is.na(percent.in.state)),]$percent.in.state <- mean(choropleth$percent.in.state)
+choropleth <- choropleth[order(choropleth$order), ]
+
+steps <- seq(from = 0.0, to = 1.0, by = 0.05)
+
+ggplot(choropleth, aes(long, lat, group = group)) +
+  geom_polygon(aes_string(fill= "percent.in.state")) +
+  geom_polygon(size=1, colour = "black", fill = NA) +
+  scale_fill_gradient(low = rgb(252,187,136, maxColorValue=255),
+                      high = rgb(215,73,25, maxColorValue=255),
+                      name = "",
+                      labels = steps,
+                      breaks = steps) +
+  labs(title = "Percent of Referrals That Stay In State") +
+  blank_theme() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 50),
+        legend.text = element_text(size = 60),
+        legend.background = element_rect(size = 60),
+        legend.key.width = unit(0.075, "npc"),
+        legend.title = element_text(size = 40))
+
+ggsave("percent-in-state-choropleth-dev.png", width=40, height = 25, units = "in")
+
+################################################################################
+#choropleth plot of percent from out of state
+state_df <- map_data("state")
+
+choropleth <- merge(state_df, all.by.state, by = "region", all.x = T)
+choropleth[with(choropleth, is.na(percent.in.state)),]$percent.in.state <- mean(choropleth$percent.in.state)
+choropleth <- choropleth[order(choropleth$order), ]
+
+steps <- seq(from = round(min(choropleth$percent.in.state), digits = 2), to = round(max(choropleth$percent.in.state), digits = 2), by = 0.05)
+
+ggplot(choropleth, aes(long, lat, group = group)) +
+  geom_polygon(aes(fill= percent.in.state), size=0.2) +
   scale_fill_gradient(low = rgb(252,187,136, maxColorValue=255),
                       high = rgb(215,73,25, maxColorValue=255),
                       name = "",
