df1a7b4b980f88e0868e2c8feae14675fb015192
Ryan Weald <ryan@weald.com>
2013-02-12 08:41:59 -0800

    Add complete hive script for taxonomy analysis

--- hive-utility-scripts/create-table-referrers.hive.sql
+++ hive-utility-scripts/create-table-referrers.hive.sql
@@ -1,17 +1,3 @@
-CREATE EXTERNAL table npi_to_taxonomy (
-  npi_number STRING,
-  entity_type INT,
-  organization_name STRING,
-  last_name STRING,
-  first_name STRING,
-  middle_name STRING,
-  prefix_text STRING,
-  taxonomy_code STRING
-)
-ROW FORMAT DELIMITED
-FIELDS TERMINATED BY ','
-LOCATION 's3://rweald-data-dumps/npi-to-taxonomy/';
-
 CREATE EXTERNAL TABLE referrals (
   referring_doc_npi STRING,
   doc_referred_to_npi STRING,
